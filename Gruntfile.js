713a9ee644a78c3c7f12362085e8b0e2ef75f69b
Rui Molefas <molefas@gmail.com>
2015-06-28 15:45:07 +0100

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,18 +24,8 @@ module.exports = function (grunt) {
 
     grunt.initConfig( configs );
 
-    grunt.registerTask('default', ['watcher']);
+    grunt.registerTask('default', ['release']);
     grunt.registerTask('watcher', ['watch:watch']);
     grunt.registerTask('release', ['concurrent:test', 'clean:release', 'concurrent:release']);
 
-    /*
-    grunt.registerTask( 'default', ['styles', 'scripts', 'html', 'copy']);
-    grunt.registerTask(  'server', ['browserSync', 'watch']);
-    grunt.registerTask(  'styles', ['clean:css', 'less:compile', 'cmq:main', 'less:sourceMap', "autoprefixer"]);
-    grunt.registerTask( 'scripts', ['jshint:beforeconcat', 'concat:main', 'concat:plugins', 'jshint:afterconcat']);
-    grunt.registerTask(    'html', ['clean:pages', 'compile-handlebars:main', 'prettify']);
-    grunt.registerTask(  'images', ['sprite', 'imagemin']);
-    grunt.registerTask( 'sprites', ['sprite:icons']);
-    grunt.registerTask(    'dist', ['default', 'cssmin', 'uglify:main', 'images']);
-    */
 };
